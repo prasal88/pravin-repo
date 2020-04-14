66826a691d5012fbbfbb3f1d7e5d2e102f915700
Pravin Rasal <pravin.rasal1@gmail.com>
2020-04-14 11:35:44 +0530

    new project commited

---
+++ doc-tracking-system/src/main/java/com/kpra/doc/app/track/DocTrackingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.kpra.doc.app.track;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocTrackingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocTrackingSystemApplication.class, args);
+	}
+
+}
---
+++ doc-tracking-system/src/test/java/com/kpra/doc/app/track/DocTrackingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kpra.doc.app.track;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocTrackingSystemApplicationTests {
+
+//	@Test
+//	void contextLoads() {
+//	}
+
+}
